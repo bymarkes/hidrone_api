9ba8060653719da64d814a23d764bfc0e7bb57ee
bymarkes <davidmarques.dp@gmail.com>
2017-05-15 15:33:15 +0200

    inner html

--- resources/views/web/map.blade.php
+++ resources/views/web/map.blade.php
@@ -89,7 +89,7 @@
 	    	document.getElementById("mySidenav").style.width = "0";
 		}else{
 	    	document.getElementById("mySidenav").style.width = "250px";
-	    	document.getElementById("mySidenav").innerHTML ='';
+	    	//document.getElementById("mySidenav").innerHTML ='';
 		}
 	}
 </script>
